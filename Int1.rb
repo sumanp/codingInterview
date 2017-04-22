9f94209137c326ca1b63fdad1646ac9ddeddad6a
sumanp <sumanpuri55@gmail.com>
2017-04-22 23:13:07 +0530

    upgrade

--- Int1.rb
+++ Int1.rb
@@ -5,20 +5,20 @@ text = gets.chomp!
 
 if text.size >= 500
   puts "Warning! Please enter 500 characters or less"
-end 
+end
 
-search_terms = ["C++", "C#", "Python", "Ruby"] + ["Physics", "Mathematics", "Chemistry", "CS"] + ["Bike", "Bus", "Rickshaw"]
+search_terms = ["C++", "C#", "Python", "Ruby"] + ["Physics", "Mathematics", "Chemistry", "CS", "Information Technology"] + ["Bike", "Bus", "Rickshaw"]
 
 def count_em(text, search_terms)
   counts = Hash.new(0)
   search_terms.each do |term|
-    counts[term] = text.split(" ").count(term)
+    counts[term] = text.scan(term).count(term)
   end
   output = counts.select{|k,v| v > 0 }
-  sorted_output = Hash[output.sort]  
+  sorted_output = Hash[output.sort]
   sorted_output.each do |k,v|
     puts "#{k} = #{v}"
-  end 
+  end
 end
 
 count_em(text, search_terms)
